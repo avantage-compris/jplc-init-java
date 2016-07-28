d13aa3eb99abd74103a625e18c29f015b337814f
jp lardy <jp.lardy@jplc.fr>
2016-07-28 11:17:52 +0200

    export2Csv

--- src/main/java/fr/jplc/Table.java
+++ src/main/java/fr/jplc/Table.java
@@ -1,5 +1,7 @@
 package fr.jplc;
 
+import java.io.*;
+
 public class Table {
 
 	public int rowCount=0;
@@ -16,10 +18,29 @@ public class Table {
 		if (rowCount<=y) rowCount=y+1;
 		if (colCount<=x) rowCount=x+1;
 	
+	}
 	
+	public void export2Csv(File f) throws IOException {
+	
+	PrintWriter writer = new PrintWriter(new FileWriter(f));
+	
+	for (int j=0; j < rowCount; j++) {
+	
+		for (int i=0; i < colCount; i++) {
+	
+			if (i>0) writer.print(',');
+			writer.print(v[i][j]);
+	
+		writer.println();	
+	
+		}	
 	
 	}
 	
+	writer.flush();
 	
+	writer.close();
+	
+	}
 	
 } //fin classe Table
