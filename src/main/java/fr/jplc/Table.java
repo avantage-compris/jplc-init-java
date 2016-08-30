0580911bdb09603b24ac59ff8913853bbdc58d6d
jp lardy <jp.lardy@jplc.fr>
2016-08-30 10:41:53 +0200

    prise en compte doubles import-export

--- src/main/java/fr/jplc/Table.java
+++ src/main/java/fr/jplc/Table.java
@@ -14,10 +14,23 @@ public class Table {
 	
 	public void setCell(int x, int y, String s) {
 	
+		/*
 		v[x][y] = s;
+		*/
+		Double dbl;
+		try {
+			dbl=Double.valueOF(s);
+			} catch(Exception e) {
+			dbl=null
+			}
+		if (dbl!=null) {
+			d[x][y]=dbl.doubleValue();
+			v[x][y]=null;
+		} else {
+			v[x][y]=s;
+		}
 		if (rowCount<=y) rowCount=y+1;
 		if (colCount<=x) colCount=x+1;
-	
 	}
 	
 	public void export2Csv(File f) throws IOException {
@@ -27,7 +40,14 @@ public class Table {
 	for (int j=0; j < rowCount; j++) {
 		for (int i=0; i < colCount; i++) {
 			if (i>0) writer.print(',');
+			/*
 			writer.print(v[i][j]);
+			*/
+			if (v[i][j]==null) {
+				writer.print(d[i][j]);
+			} else {
+				writer.print(v[i][j]);
+			}
 		}	
 	writer.println();	
 	}
