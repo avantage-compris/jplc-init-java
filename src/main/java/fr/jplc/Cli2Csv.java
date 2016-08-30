a08e66f665632305443f967376779cf902502a36
jp lardy <jp.lardy@jplc.fr>
2016-08-30 12:25:36 +0200

    covm:compile

--- src/main/java/fr/jplc/Cli2Csv.java
+++ src/main/java/fr/jplc/Cli2Csv.java
@@ -1,5 +1,7 @@
 package fr.jplc;
 
+import java.io.File;
+
 public class Cli2Csv {
 
 	/* point d'entree obligatoire */
@@ -28,6 +30,12 @@ public class Cli2Csv {
 	
 	t.export2Csv(outputFile);
 	
+	Table t2 = covm(t, 1, 60);
+	
+	File f2 = new File("target/covm.csv");
+	
+	t2.export2Csv(f2);
+		
 	/*
 	java.io.PrintWriter writer = new java.io.PrintWriter(new java.io.FileWriter(outputFile));
 	writer.println(new java.util.Date() + ",1");
@@ -66,5 +74,35 @@ public class Cli2Csv {
 			}
 		}
 	}
+	
+	private static Table covm(Table t, double lda0, int stp) {
+	
+		double y[][] = new double [t.colCount][t.rowcount];
+	
+		double lda=0.0;
+		double temp=0.0;
+	
+		Table u = new Table();
+		
+		for (int k=stp; k<t.rowCount; k++) {	
+			lda=0.0;
+			for (int i=0; i<t.colCount; i++) {
+				for (int j=0; j<=i; j++) {
+					temp = (t.getDoubleValue(i,k) - t.getDoubleValue(i,k-stp)) * (t.getDoubleValue(j,k) - t.getDoubleValue(j,k-stp)) ;
+					y[i][j]= lda * y[i][j] + (1.0 - lda) * temp ;
+					y[j][i] = y[i][j] ;
+				}
+			}
+			lda = 1.0 / (2.0 - lda);
+			if (lda > lda0) {lda = lda0};
+		}
+		for (int i=0; i<t.colCount; i++) {
+			for (int j=0; j<t.rowCount; j++) {
+				u.setCell(i, j, y[i][j]);
+			}
+		}
+		return u;			
+	}
+	
 	
 } //fin classe Cli2Csv
